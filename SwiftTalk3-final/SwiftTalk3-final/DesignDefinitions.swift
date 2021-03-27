5bf93dc54cd103275423c2c83f0df4a331f52bef
Anditto Heristyo <anditto.heristyo@gmail.com>
2021-03-27 23:36:55 +0900

    Added DesignDefinitions

---
+++ SwiftTalk3-final/SwiftTalk3-final/DesignDefinitions.swift
@@ -0,0 +1,13 @@
+//
+//  DesignDefinitions.swift
+//  SwiftTalk3-final
+//
+//  Created by anditto-heristyo on 2021/03/27.
+//
+
+import SwiftUI
+
+extension Color {
+    static let bgColor = Color(red: 33/255.0, green: 33/255.0, blue: 55/255.0)
+    static let pocketColor = Color(red: 40/255.0, green: 42/255.0, blue: 68/255.0)
+}
---
+++ SwiftTalk3-final/SwiftTalk3-finalTests/SwiftTalk3_finalTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftTalk3_finalTests.swift
+//  SwiftTalk3-finalTests
+//
+//  Created by anditto-heristyo on 2021/03/27.
+//
+
+import XCTest
+@testable import SwiftTalk3_final
+
+class SwiftTalk3_finalTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
